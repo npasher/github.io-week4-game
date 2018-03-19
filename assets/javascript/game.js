55c4e72812c8153b22bf21c9307ddef78426ceb7
npasher <n.p.asher@gmail.com>
2018-03-19 18:22:53 -0400

    adjusting audio/psuedo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-
+  /*assigns audio to button press*/
   var audioElement=document.createElement("audio");
         audioElement.setAttribute("src", "assets/audio/8BallAitken_CycloneCountry.mp3");
-
+  /*audio on button press*/
   $(".crystal").click(function(){
         audioElement.play();
         });
@@ -10,34 +10,36 @@ $(document).ready(function() {
         audioElement.pause();
         });
 
+/*assigns variable random number generator*/
   var randomNum=Math.floor(Math.random()*102)+19;
   $("#target").html(randomNum);
-
+/*adjusts number total with selection*/
   var currentScore=0;
   var newScore=$("<div>");
   newScore.html(currentScore);
   $("#score-current").append(newScore);
-
+/*win total*/
   var winCount=0;
   $("#wins").html(winCount);
+/*loss total*/
   var lossCount=0;
   $("#losses").html(lossCount);
-
+/*generates button value*/
   var tweedVal=$("#tweed").val(Math.floor(Math.random()*12)+1);
   var greenVal=$("#green").val(Math.floor(Math.random()*12)+1);
   var greyVal=$("#grey").val(Math.floor(Math.random()*12)+1);
   var yellowVal=$("#yellow").val(Math.floor(Math.random()*12)+1);
-
+/*calls button value*/
   tweedVal=$("#tweed").val();
   greenVal=$("#green").val();
   greyVal=$("#grey").val();
   yellowVal=$("#yellow").val();
-
+/*compares value of click with random number*/
   $(".crystal").click(function(){
     currentScore=currentScore+parseInt($(this).val());
     newScore.html(currentScore);
     $("#score-current").append(newScore);
-
+/*loss function*/
     if(currentScore>randomNum){
       lossCount++;
       $("#losses").html(lossCount);
@@ -62,7 +64,7 @@ $(document).ready(function() {
       yellowVal= $("#yellow").val();
 
     }
-
+/*win function*/
     if(currentScore === randomNum){
       winCount++;
       $("#wins").html(winCount);
